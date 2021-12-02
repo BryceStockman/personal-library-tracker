5c49f14298b92df99e8d5293558676205318d999
Jessica <sevorg.jess@gmail.com>
2021-12-01 20:45:19 -0700

    create search result logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
-// Link to open library 2 APIS
-fetch('http://openlibrary.org/search.json?q=the+lord+of+the+rings')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-  // Covers URL, 6 digit number is the cover_i - SIZE
-  // https://covers.openlibrary.org/b/id/269727-M.jpg
-  
+var ulEl = document.getElementById('search-results');
 
   // Free Dictionary API
   fetch('https://api.dictionaryapi.dev/api/v2/entries/en/chameleon')
@@ -16,35 +9,68 @@ fetch('http://openlibrary.org/search.json?q=the+lord+of+the+rings')
 // Look into URI encoding to handle spaces
 formEl = document.querySelector('#search-box')
 
-// store input value as a variable, remove spaces in second variable
+// store input value as a variable
 var getInputValue = function() {
 var inputVal = document.getElementById("search-input").value;
+// remove spaces from inputVal
 var urlReadyValue = encodeURIComponent(inputVal);
 
 console.log(urlReadyValue)
 
-
-
-  var apiSearchUrl = "http://openlibrary.org/search.json?q=" + urlReadyValue;
+  var apiSearchUrl = "http://openlibrary.org/search.json?q=" + urlReadyValue + "&fields=title,author_name,key,cover_i,number_of_pages_median,&limit=5";
 
   fetch(apiSearchUrl)
   .then(function(response) {
     if(response.ok) {
       response.json().then(function(data) {
-        console.log(data.docs[0].cover_i)
+        var pageCoverId = data.docs[0].cover_i
+        console.log(pageCoverId)
+        var imgEl = document.getElementById('bookImg')
+        // imgEl.innerHTML = 'https://covers.openlibrary.org/b/id/' + pageCoverId + '-M.jpg'
 
+        // display book cover
+        imgEl.setAttribute('src', 'https://covers.openlibrary.org/b/id/' + pageCoverId + '-M.jpg')
         
-      
-      })
+        // var showSearchResults = function() {
+        //   var searchResults = []
+        //   searchResults.push({})
+
+        var searchResults = data.docs
+        console.log(searchResults)
+
+        searchResults.forEach(function(result){
+          var title = result?.title
+          var author = result?.author_name?.[0]
+          var coverId = result?.cover_i
+          var pages = result?.number_of_pages_median
+          var key = result?.key
+          var criteria ="title: " + title + " | author: " + author + " | pages: " + pages
+
+          var liEl = document.createElement('li')
+          ulEl.appendChild(liEl)
+          liEl.textContent = criteria
+
+
+          console.log(title, author, coverId, pages, key)
+        })
+        }
+   ) }
     }
-  })
-}
+  )}
+//}
+
+// var showSearchResults = function() {
+// var searchResults = []
+// searchResults.push({})
+// }
 
 
 
 
 
-// var urlUri = encodeURIComponent(searchValue)
+
+
+
 
 // Search API to find books - parse data for needed fields (determine what they are)
 
